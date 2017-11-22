1261a03452c2304e18b92965dce417db5cf75b21
saju10 <shajahansj@gmail.com>
2017-11-22 16:43:21 +0600

    commit hoya ja

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*
+    Author 		: Md. Shajahan Kabir Saju
+	Create Date : 22 November, 2017
+	Description : Create Asset Bundle for a 3d model. also kept this script into editor folder
+*/
+
+public class CreateAssetBundle : Editor {
+
+	[MenuItem("Bundle/Make Asset Bundle")]
+	static void  BundleAllAssets(){
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+
+}
---
+++ Assets/Script/Load3dAssets.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+    Author 		: Md. Shajahan Kabir Saju
+	Create Date : 22 November, 2017
+	Description : Loading 3d asset(asset bundle) into run time from sd card or phone memory
+	Note that: Set Write Access = External (SDCard) into player setting.
+*/
+public class Load3dAssets : MonoBehaviour {
+	
+	public Text showText;
+
+	void Start () {
+		StartCoroutine (FuncLoadAssets ());
+	}
+
+	IEnumerator FuncLoadAssets(){
+		while(!Caching.ready)
+			yield return null;
+		//aa is a 3d asset bundle
+		//for phone memory
+		//paste aa asset bundle into files folder that is exist in yor packege name
+		//aa is a bundle name
+		string bundleURL ="file://"+ Application.persistentDataPath+"/aa";
+		//for sd card
+		//Create a folder into your sd card named my3dasset. Paste aa bundle into my3dasset folder.
+		//string bundleURL = "file:///mnt/sdcard/my3dasset/aa";
+		using (WWW www = WWW.LoadFromCacheOrDownload (bundleURL, 1)) {
+			yield return www;
+			if (www.error == "") {
+				AssetBundle bundle = www.assetBundle;
+				//aa is object name not bundle name
+				GameObject temp = (GameObject)bundle.LoadAsset ("aa");
+				Instantiate (temp, new Vector3 (0, 0, 0), Quaternion.identity);
+			} else {
+				showText.text = "Error: "+www.error+":";
+				throw new UnityException ("WWW Download had an error: " + www.error);
+			}
+		}
+
+
+	}
+
+
+}
